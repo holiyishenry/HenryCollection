ef12e437c8727e56838fc6dcf9b9486e9465e6a7
henry <holiyjq@163.com>
2015-01-06 17:47:58 +0800

    2015、1、6自定义动画，颜色变化进度条

---
+++ divprogressbar/src/main/java/com/henry/divprogressbar/ImageProgressActivity.java
@@ -0,0 +1,15 @@
+package com.henry.divprogressbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by holiy on 2015/1/6.
+ */
+public class ImageProgressActivity  extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_progress_activity);
+    }
+}
